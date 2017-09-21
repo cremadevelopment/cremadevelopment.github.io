59be17d9cff6376c1dc75eb6dbb511af3f349e16
Carter Barnes <carterabarnes@gmail.com>
2017-09-20 19:34:46 -0700

    add jekyll build script to scripts

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,5 +1,1 @@
-. .\scripts\docker-down.ps1
-
-docker run --rm --volume=${PWD}:/srv/jekyll -p 4000:4000 jekyll/jekyll jekyll build
-
-docker run --name cremasite --rm -d -v ${PWD}\_site:/usr/share/nginx/html -p 8080:80 nginx
+docker run --rm --volume=${PWD}:/srv/jekyll jekyll/jekyll jekyll build
